de75f1b6900b1ab0580426b957d7b319d0a042d0
Mariano Cardoso <marianocardoso92@gmail.com>
2024-09-21 00:40:53 -0300

    Primera Subida

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EJERCICIO_2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PROMEDIO DE 3 NOTAS DE UN ALUMNO
+            //PASO 0: DECLARA VARIABLES
+            float N1, N2, N3, PROM;
+
+            //PASO 1: INGRESAR NOTAS
+            Console.WriteLine("INGRESE LA PRIMER NOTA: ");
+            N1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE LA SEGUNDA NOTA: ");
+            N2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE LA TERCER NOTA: ");
+            N3 = float.Parse(Console.ReadLine());
+            //PASO 2: CALCULAR PROMEDIO 
+            PROM = (N1 + N2 + N3) / 3;
+
+            //PASO 3: MOSTRAR EN PANTALLA
+            Console.WriteLine("EL PROMEDIO DEL ALUMNO ES: " + PROM.ToString("0.00"));
+        }
+    }
+}
